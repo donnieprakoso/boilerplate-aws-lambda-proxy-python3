fdc6883ca6d11a4ae46ef38c523a8c36a3fc23b1
Donnie Prakoso <donnie.prakoso@gmail.com>
2017-08-29 12:26:17 +0700

    Updating README and adding few comments in main.py

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# boilerplate-aws-lambda-proxy-python3
+This repo serves Python 3 boilerplate for AWS Lambda with proxy integration.
+
+## Objective
+The main objective for this boilerplate is to provide a quick start for AWS Lambda Python3 development. This boilerplate starts with a minimal requirement, by only importing Python built in JSON package.
+
+Also, please be noted that this repo is designed to accept the request from proxy integration and not suitable for a regular AWS Lambda function due to differences in 'event' variable.
+
+## Resource Links 
+- [AWS API Gateway Documentation](http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-set-up-simple-proxy.html#api-gateway-simple-proxy-for-lambda-input-format) . 
+- [HTTP Status Code](https://httpstatuses.com/)
+
+## License
+This project is licensed under the MIT license. See LICENSE for details.
--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 import json
+
 '''
+Python 3 Boilerplate for AWS Lambda Proxy Integration
+https://github.com/donnieprakoso/boilerplate-aws-lambda-proxy-python3
+
+This boilerplate refers to AWS documentation, link as below:
 http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-set-up-simple-proxy.html#api-gateway-simple-proxy-for-lambda-input-format
-{
-    "resource": "Resource path",
-    "path": "Path parameter",
-    "httpMethod": "Incoming request's method name"
-    "headers": {Incoming request headers}
-    "queryStringParameters": {query string parameters }
-    "pathParameters":  {path parameters}
-    "stageVariables": {Applicable stage variables}
-    "requestContext": {Request context, including authorizer-returned key-value pairs}
-    "body": "A JSON string of the request payload."
-    "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
-}
 '''
 
 '''
+Request JSON format for proxy integration
 {
-    "isBase64Encoded": true|false,
-    "statusCode": httpStatusCode,
-    "headers": { "headerName": "headerValue", ... },
-    "body": "..."
+	"resource": "Resource path",
+	"path": "Path parameter",
+	"httpMethod": "Incoming request's method name"
+	"headers": {Incoming request headers}
+	"queryStringParameters": {query string parameters }
+	"pathParameters":  {path parameters}
+	"stageVariables": {Applicable stage variables}
+	"requestContext": {Request context, including authorizer-returned key-value pairs}
+	"body": "A JSON string of the request payload."
+	"isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+}
+
+
+Response JSON format
+{
+	"isBase64Encoded": true|false,
+	"statusCode": httpStatusCode,
+	"headers": { "headerName": "headerValue", ... },
+	"body": "..."
 }
 
 '''
 
 def response_proxy(data):
-  response = {}
-  response["isBase64Encoded"] = False
-  response["statusCode"] = data["statusCode"]
-  response["headers"] = {}
-  if "headers" in data:
-    response["headers"] = data["headers"]
-  response["body"] = json.dumps(data["body"])
-  return response
+	'''
+	For HTTP status codes, you can take a look at https://httpstatuses.com/
+	'''
+	response = {}
+	response["isBase64Encoded"] = False
+	response["statusCode"] = data["statusCode"]
+	response["headers"] = {}
+	if "headers" in data:
+	response["headers"] = data["headers"]
+	response["body"] = json.dumps(data["body"])
+	return response
 
 def request_proxy(data):
-  request = {}
-  request = data
-  request["body"]=json.loads(data["body"])
-  return request
+	request = {}
+	request = data
+	request["body"]=json.loads(data["body"])
+	return request
 
 def handler(event, context):
 	response = {}
@@ -48,7 +60,9 @@ def handler(event, context):
 		response["statusCode"]=200
 		response["headers"]={}
 		'''
-		data["headers"]["Access-Control-Allow-Methods"]="POST,OPTIONS"
+		Default headers for CORS
+
+		data["headers"]["Access-Control-Allow-Methods"]="GET,POST,OPTIONS"
 		data["headers"]["Access-Control-Allow-Headers"]="Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
 		data["headers"]["Access-Control-Allow-Origin"]="*"
 		'''		
